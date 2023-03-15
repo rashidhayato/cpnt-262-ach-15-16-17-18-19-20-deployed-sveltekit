f49ab0c82e0fd549a106ff6b4262a94ac8f45e50
rashid <abdulrashid.hayato@edu.sait.ca>
2023-03-14 23:29:15 -0600

    adding new repo

---
+++ deployed/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+   import './styles.css';
+</script>
+
+
+
+<nav>
+  <ul>
+    <a href="about"> About</a>
+    <a href="service"> Service</a>
+    <a href="menu"> menu </a>
+  </ul>
+
+</nav>
+
+
+
+
+
+<slot></slot>
---
+++ deployed/src/routes/shop/+page.svelte
@@ -0,0 +1,6 @@
+<h1>
+  Rashids online shopping web
+</h1>
+<p>
+  Welcome to my shopping page 
+</p>
